55e6b4b61ce81b7c30b154dfb0e6b960a5dc301e
Chernyshov <xpom55@mail.ru>
2019-02-15 09:42:54 +0300

    Send email on post

--- build/main.js
+++ build/main.js
@@ -135,12 +135,62 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var koa_bodyparser__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! koa-bodyparser */ "koa-bodyparser");
 /* harmony import */ var koa_bodyparser__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(koa_bodyparser__WEBPACK_IMPORTED_MODULE_1__);
 
+ // import Email from 'email-templates';
 
-const router = new koa_router__WEBPACK_IMPORTED_MODULE_0___default.a(); // curl -H "Content-Type: application/json" -X POST --data '{"value": "30"}' http://localhost:3334
+const nodemailer = __webpack_require__(/*! nodemailer */ "nodemailer"); // const nodemailerSendgrid = require('nodemailer-sendgrid');
+
+
+const router = new koa_router__WEBPACK_IMPORTED_MODULE_0___default.a(); // async..await is not allowed in global scope, must use a wrapper
+
+async function main(mailObject) {
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  //   let account = await nodemailer.createTestAccount();
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.mail.ru',
+    port: 587,
+    secure: false,
+    // true for 465, false for other ports
+    auth: {
+      //   user: account.user, // generated ethereal user
+      //   pass: account.pass, // generated ethereal password
+      user: 'xpom66nikita004@mail.ru',
+      // generated ethereal user
+      pass: '1qazxsw2' // generated ethereal password
+
+    }
+  }); // setup email data with unicode symbols
+
+  let mailOptions = {
+    from: '"Fred Foo 👻" <xpom66nikita004@mail.ru>',
+    // sender address
+    to: 'xpom5566@mail.ru',
+    // list of receivers
+    subject: `Hello ✔ ${mailObject.number}`,
+    // Subject line
+    text: `Hello world? ${mailObject.number}`,
+    // plain text body
+    html: `<b>Hello world?${JSON.stringify(mailObject)}</b>` // html body
+
+  }; // send mail with defined transport object
+
+  let info = await transporter.sendMail(mailOptions);
+  console.log('Message sent: %s', info.messageId); // Preview only available when sending through an Ethereal account
+
+  console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info)); // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+} // curl -H "Content-Type: application/json" -X POST --data '{"value": "30"}' http://localhost:3334
+
 
 router.post('/', koa_bodyparser__WEBPACK_IMPORTED_MODULE_1___default()(), async ctx => {
   console.log('asdfl alskdfj laskjdf test');
-  console.log('request', ctx.request.body);
+  const {
+    body
+  } = ctx.request; //   sendEmail();
+
+  main(body).catch(console.error);
+  console.log('request', body);
   const data = {
     allGood: true
   };
@@ -228,6 +278,17 @@ module.exports = require("koa-morgan");
 /***/ (function(module, exports) {
 
 module.exports = require("koa-router");
+
+/***/ }),
+
+/***/ "nodemailer":
+/*!*****************************!*\
+  !*** external "nodemailer" ***!
+  \*****************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("nodemailer");
 
 /***/ })
 
--- src/router.js
+++ src/router.js
@@ -1,17 +1,64 @@
 import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 
+// import Email from 'email-templates';
+const nodemailer = require('nodemailer');
+// const nodemailerSendgrid = require('nodemailer-sendgrid');
+
 const router = new Router();
+
+// async..await is not allowed in global scope, must use a wrapper
+async function main(mailObject) {
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  //   let account = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.mail.ru',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      //   user: account.user, // generated ethereal user
+      //   pass: account.pass, // generated ethereal password
+      user: 'xpom66nikita004@mail.ru', // generated ethereal user
+      pass: '1qaz', // generated ethereal password
+    },
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+    from: '"Fred Foo 👻" <xpom66nikita004@mail.ru>', // sender address
+    to: 'xpom5566@mail.ru', // list of receivers
+    subject: `Hello ✔ ${mailObject.number}`, // Subject line
+    text: `Hello world? ${mailObject.number}`, // plain text body
+    html: `<b>Hello world?${JSON.stringify(mailObject)}</b>`, // html body
+  };
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail(mailOptions);
+
+  console.log('Message sent: %s', info.messageId);
+  // Preview only available when sending through an Ethereal account
+  console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
 
 // curl -H "Content-Type: application/json" -X POST --data '{"value": "30"}' http://localhost:3334
 router.post('/', bodyParser(), async (ctx) => {
-    console.log('asdfl alskdfj laskjdf test');
+  console.log('asdfl alskdfj laskjdf test');
 
-    console.log('request', ctx.request.body);
+  const { body } = ctx.request;
+  //   sendEmail();
+  main(body).catch(console.error);
 
-    const data = { allGood: true };
-    ctx.status = 200;
-    ctx.body = JSON.stringify({ data });
+  console.log('request', body);
+
+  const data = { allGood: true };
+  ctx.status = 200;
+  ctx.body = JSON.stringify({ data });
 });
 
 export default router;
